b9317bd09f22665c933300f05c52388d524b7def
Jeremy Leland <jeremy.leland@gmail.com>
2023-05-05 21:58:08 -0700

    Went back to the simpler version of everything, since it works pretty well for avoiding rocks.
    Will try to have ships flock separately

--- v2/src/AvoidCones.js
+++ v2/src/AvoidCones.js
@@ -3,70 +3,35 @@ import * as Util from './Util.js';
 export class AvoidCones {
   cones = [];
 
-  // TODO: Add these once in the constructor, and finalize once there?
-  // Use filter to remove cones that collide (while updating newCone), then add newCone?
+  constructor( cones ) {
+    cones.forEach( newCone => {
+      this.cones = this.cones.filter( other => {
+        let merge = false;
 
-  constructor( closedCones ) {
-    if ( closedCones.length == 0 ) {
-      this.cones.push( {
-        left: -Math.PI,
-        right: Math.PI,
-        open: true,
-      } );
-    }
-    else { 
-      closedCones.forEach( newCone => {
-        this.cones = this.cones.filter( other => {
-          let merge = false;
+        if ( Util.betweenAngles( newCone.left, other.left, other.right ) ) {
+          newCone.left = other.left;
+          merge = true;
+        }
+  
+        if ( Util.betweenAngles( newCone.right, other.left, other.right ) ) {
+          newCone.right = other.right;
+          merge = true;
+        }
+  
+        if ( Util.betweenAngles( other.left, newCone.left, newCone.right ) && 
+            Util.betweenAngles( other.right, newCone.left, newCone.right ) ) {
+          merge = true;
+        }
 
-          if ( Util.betweenAngles( newCone.left, other.left, other.right ) ) {
-            newCone.left = other.left;
-            merge = true;
-          }
-    
-          if ( Util.betweenAngles( newCone.right, other.left, other.right ) ) {
-            newCone.right = other.right;
-            merge = true;
-          }
-    
-          if ( Util.betweenAngles( other.left, newCone.left, newCone.right ) && 
-              Util.betweenAngles( other.right, newCone.left, newCone.right ) ) {
-            merge = true;
-          }
-
-          return !merge;
-        } );
-
-        this.cones.push( newCone );
+        return !merge;
       } );
 
-      this.cones.sort( ( a, b ) => a.left - b.left );
-
-      const combined = [];
-
-      for ( let i = 0; i < this.cones.length; i ++ ) {
-        combined.push( this.cones[ i ] );
-        combined.push( {
-          left: this.cones[ i ].right,
-          right: this.cones[ ( i + 1 ) % this.cones.length ].left,
-          open: true,
-        } );
-      }
-
-      this.cones = combined;
-    }
+      this.cones.push( newCone );
+    } );
   }
 
   getCone( angle ) {
-    const matches = this.cones.filter( c => Util.betweenAngles( angle, c.left, c.right ) );
-
-    // If we are on edge of two cones, consider us part of the open one
-    if ( matches.length > 1 ) {
-      return matches.find( c => c.open );
-    }
-    else {
-      return matches[ 0 ];
-    }
+    return this.cones.find( c => Util.betweenAngles( angle, c.left, c.right ) );
   }
 
   draw( ctx, scale = 100 ) {
